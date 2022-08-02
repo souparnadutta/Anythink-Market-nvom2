cd21c9a382bf19b4b76cac7affdde76bc02b2f2e
souparnadutta <70193389+souparnadutta@users.noreply.github.com>
2022-08-02 13:28:10 +0530

    Bug Fixed

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -36,7 +36,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={item.image} onError={(e)=>{e.target.onerror = null; e.target.src="placeholder.png"}}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
